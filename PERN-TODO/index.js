dabcd6156250e3e92ed86075b9956e05b744a840
James Torres <james.torres01@gmail.com>
2021-12-17 12:03:22 -0500

    new token terminal

--- PERN-TODO/index.js
+++ PERN-TODO/index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+// process.env.PORT
+// process.env.NODE_ENV => production
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    // server static content
+    // npm run build
+    app.use(express.static(path.join(__dirname, "react/build")))
+}
 
 // routes //
 
@@ -69,6 +80,6 @@ app.delete("/todos/:id", async (req, res) => {
 });
 
 // listener
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
     console.log("server is live");
 })
